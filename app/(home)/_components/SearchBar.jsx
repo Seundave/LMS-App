199199e17bef2e69a9adf5d139085408ed7e4225
Seundave <olayinkadavid62@yahoo.com>
2024-04-23 10:52:35 +0100

    changed the home route

--- app/(home)/_components/SearchBar.jsx
+++ app/(home)/_components/SearchBar.jsx
@@ -4,8 +4,12 @@ import React from "react";
 function SearchBar() {
   return (
     <div className="flex gap-3 text-[14px] items-center border p-2 rounded-md bg-gray-50 text-gray-500">
-      <Search height={17}/>
-      <input type="text" placeholder="Search Course" className="bg-transparent outline-none"/>
+      <Search height={17} />
+      <input
+        type="text"
+        placeholder="Search Course"
+        className="bg-transparent outline-none"
+      />
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,31 @@
-import Image from 'next/image'
-import { UserButton } from '@clerk/nextjs'
+// import Image from 'next/image'
+// import { UserButton } from '@clerk/nextjs'
+
+// export default function Home() {
+//   return (
+//    <div className="">
+//     Home
+//     <UserButton afterSignOutUrl="/"/>
+//    </div>
+//   )
+// }
+
+
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/browse');
+  }, []); // Empty dependency array ensures this effect runs only once when the component mounts
+
   return (
    <div className="">
-    Home
     <UserButton afterSignOutUrl="/"/>
    </div>
-  )
+  );
 }
